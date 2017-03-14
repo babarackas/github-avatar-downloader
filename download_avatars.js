332ede47548c7041a0eadd631143e11750cc8952
Philip <pbazzard@gmail.com>
2017-03-14 00:36:06 +0000

    added request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require('request');
+var GITHUB_USER = 'babarackas';
+var GITHUB_TOKEN = 'b8466eba95146abad5189381d14bc7fefc12ed15';
+var repoOwner = 'jquery';
+var repoName = 'jquery';
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+console.log(requestURL);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//getRepoContributors("jquery", "jquery", function(err, result) {
+  //console.log("Errors:", err);
+  //console.log("Result:", result);
+//});
+
