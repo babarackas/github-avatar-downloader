1bb6b5ad177750f424aba5afe13102a19f21b489
Philip <pbazzard@gmail.com>
2017-03-14 02:12:47 +0000

    parsing json data

--- download_avatars.js
+++ download_avatars.js
@@ -3,15 +3,29 @@ var GITHUB_USER = 'babarackas';
 var GITHUB_TOKEN = 'b8466eba95146abad5189381d14bc7fefc12ed15';
 var repoOwner = 'jquery';
 var repoName = 'jquery';
-
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-console.log(requestURL);
+var options =
+  {
+    url: requestURL,
+    headers: {
+    'User-Agent' : 'GitHub Avatar Downloader - Student Project'
+    }
+  };
 
-console.log('Welcome to the GitHub Avatar Downloader!');
 
-//getRepoContributors("jquery", "jquery", function(err, result) {
-  //console.log("Errors:", err);
-  //console.log("Result:", result);
-//});
+function getRepoContributors(repoOwner, repoName, cb) {
+  request(options, function (error, response, body) {
+    var data = JSON.parse(body);
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('data', data); // Print the HTML for the Google homepage.
+});
 
+  }
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
